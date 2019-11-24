0f82a089f578b95b1039cd07991b1fc5e415757a
Ying Wang <yingw787.personal@gmail.com>
2019-11-23 23:55:12 -0500

    Update part_1.py.

--- part_1.py
+++ part_1.py
@@ -12,10 +12,20 @@ will need to create in order to serve queries about the questions above. List
 out all of the table names and columns in a text file to be submitted with the
 homework. Additionally, create a SQLite database and create the schema/tables
 that you designed in that database.
+
+__________
+
+NOTE: Python module `sqlite3` connection and cursor objects do not implement
+method `__enter__`, which means that they cannot be used as part of a context
+manager. This may embrittle the process of closing connections in the event of
+OS interrupts to Python process. However, since SQLite is a file and not
+necessarily a separate daemon, this possibility may be mitigated or different
+entirely from e.g. PostgreSQL.
 """
 
 from __future__ import absolute_import
 
+import contextlib
 import os
 
 import sqlite3
@@ -48,6 +58,8 @@ def generate_sqlite_schema():
         os.remove(database_abspath)
 
     sqlite3_conn = sqlite3.connect(database)
+    sqlite3_cursor = sqlite3_conn.cursor()
+
     sqlite3_conn.commit()
 
     # Create SQL statements to be run.
@@ -60,7 +72,7 @@ def generate_sqlite_schema():
         weight INTEGER NOT NULL
     );
     '''
-    sqlite3_conn.execute(pokemon_create_table_sql)
+    sqlite3_cursor.execute(pokemon_create_table_sql)
     sqlite3_conn.commit()
 
     moves_create_table_sql = '''
@@ -71,14 +83,72 @@ def generate_sqlite_schema():
         accuracy INTEGER NOT NULL
     );
     '''
-    sqlite3_conn.execute(moves_create_table_sql)
+    sqlite3_cursor.execute(moves_create_table_sql)
     sqlite3_conn.commit()
+
+    sqlite3_cursor.close()
+    sqlite3_conn.close()
 
 
 def generate_txt_file_from_sqlite_schema():
     """Generates a .txt file from a SQLite schema.
+
+    Using a method to derive the answer from the created SQLite schemas in order
+    to guarantee consistency between actual/stated schema.
     """
-    pass
+    txt_filename = 'schemas.txt'
+    txt_abspath = os.path.abspath(os.path.join(
+        os.path.dirname(__file__),
+        txt_filename
+    ))
+    if os.path.exists(txt_abspath):
+        os.remove(txt_abspath)
+
+    database = 'pokemon.db'
+    sqlite3_conn = sqlite3.connect(database)
+    sqlite3_cursor = sqlite3_conn.cursor()
+
+    file_descriptor = open(txt_abspath, 'w')
+
+    file_descriptor_intro = '''
+    This file describes the schemas of tables used as part of the Compass data
+    engineering challenge.
+
+    Database is "pokemon.db".
+
+    Tables as follows:
+    '''
+    file_descriptor.write(file_descriptor_intro)
+    file_descriptor.flush()
+
+    describe_pokemon_sql = '''
+    SELECT sql
+    FROM sqlite_master
+    WHERE name = 'pokemon';
+    '''
+    sqlite3_cursor.execute(describe_pokemon_sql)
+    result = sqlite3_cursor.fetchone()[0]
+    file_descriptor.write('\n')
+    file_descriptor.write(result)
+    file_descriptor.write('\n')
+    file_descriptor.flush()
+
+    describe_moves_sql = '''
+    SELECT sql
+    FROM sqlite_master
+    WHERE name = 'moves';
+    '''
+    sqlite3_cursor.execute(describe_moves_sql)
+    result = sqlite3_cursor.fetchone()[0]
+    file_descriptor.write('\n')
+    file_descriptor.write(result)
+    file_descriptor.write('\n')
+    file_descriptor.flush()
+
+    sqlite3_cursor.close()
+    sqlite3_conn.close()
+    file_descriptor.close()
+
 
 # Wrapping in order to avoid having to eagerly loading entire module at import
 # time.
