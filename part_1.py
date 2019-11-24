5b43e8804355360180fc035cd106cbb3c6377468
Ying Wang <yingw787.personal@gmail.com>
2019-11-23 23:33:26 -0500

    Update part_1.py first order approximation for DB models.

--- part_1.py
+++ part_1.py
@@ -13,3 +13,75 @@ out all of the table names and columns in a text file to be submitted with the
 homework. Additionally, create a SQLite database and create the schema/tables
 that you designed in that database.
 """
+
+from __future__ import absolute_import
+
+import os
+
+import sqlite3
+
+
+def generate_sqlite_schema():
+    """Generates the SQLite schema according to problem requirements.
+    """
+    # The problems involve keeping track of:
+    # - Pokemon
+    #   - Weight
+    #   - Type
+    #
+    # - Moves
+    #   - Type
+    #   - Accuracy
+    #
+    # With a JOIN issued on type between the two tables.
+
+    # Define variables to be used as part of the method.
+    database = 'pokemon.db'
+
+    # Delete the database if it already exists, in order to ensure idempotency
+    # from stateful method call with side effects.
+    database_abspath = os.path.abspath(os.path.join(
+        os.path.dirname(__file__),
+        database
+    ))
+    if os.path.exists(database_abspath):
+        os.remove(database_abspath)
+
+    sqlite3_conn = sqlite3.connect(database)
+    sqlite3_conn.commit()
+
+    # Create SQL statements to be run.
+    pokemon_create_table_sql = '''
+    CREATE TABLE pokemon
+    (
+        id INTEGER PRIMARY KEY NOT NULL,
+        name NVARCHAR(256) NOT NULL,
+        type NVARCHAR(256) NOT NULL,
+        weight INTEGER NOT NULL
+    );
+    '''
+    sqlite3_conn.execute(pokemon_create_table_sql)
+    sqlite3_conn.commit()
+
+    moves_create_table_sql = '''
+    CREATE TABLE moves
+    (
+        id INTEGER PRIMARY KEY NOT NULL,
+        type NVARCHAR(256) NOT NULL,
+        accuracy INTEGER NOT NULL
+    );
+    '''
+    sqlite3_conn.execute(moves_create_table_sql)
+    sqlite3_conn.commit()
+
+
+def generate_txt_file_from_sqlite_schema():
+    """Generates a .txt file from a SQLite schema.
+    """
+    pass
+
+# Wrapping in order to avoid having to eagerly loading entire module at import
+# time.
+if __name__=='__main__':
+    generate_sqlite_schema()
+    generate_txt_file_from_sqlite_schema()
