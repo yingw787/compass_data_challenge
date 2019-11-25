8037932ca065cb93c4130a2d0f79a81d0b3a6d26
Ying Wang <yingw787.personal@gmail.com>
2019-11-25 13:17:07 -0500

    Update utils.py adding additional validation methods.

--- utils.py
+++ utils.py
@@ -6,10 +6,27 @@ from __future__ import absolute_import
 import os
 import re
 
-import sqlite3
+
+def database_exists():
+    """Validates that the database file exists.
+
+    Returns:
+        bool: Absolute file path to the hardcoded database file exists.
+    """
+    # Check that the database file exists. #
+    expected_db_name = 'pokemon.db'
+    expected_db_abspath = os.path.abspath(os.path.join(
+        os.path.dirname(__file__),
+        expected_db_name
+    ))
+
+    return os.path.exists(expected_db_abspath)
 
 
-def pokemon_table_schema_is_valid():
+def pokemon_table_schema_is_valid(
+        sqlite3_conn,
+        sqlite3_cursor
+):
     """Validates SQLite table 'pokemon' and panics if result table schema
     does not match the expected table schema.
 
@@ -17,20 +34,14 @@ def pokemon_table_schema_is_valid():
     is, ensure alignment between stateful db table generation, and stateful
     ingestion from third-party API, and checkpoint the data pipeline to
     diagnose any failures.
+
+    Args:
+        sqlite3_conn (sqlite3.Connection)
+        sqlite3_cursor (sqlite3.Cursor)
+
+    Returns:
+        bool: SQLite table 'pokemon' exists and has expected schema.
     """
-    # Check that the database file exists. #
-    expected_table_schema = ''
-    expected_db_name = 'pokemon.db'
-    expected_db_abspath = os.path.abspath(os.path.join(
-        os.path.dirname(__file__),
-        expected_db_name
-    ))
-    if not os.path.exists(expected_db_abspath):
-        return False
-
-    sqlite3_conn = sqlite3.connect(expected_db_name)
-    sqlite3_cursor = sqlite3_conn.cursor()
-
     # Check that the table 'pokemon' exists. #
     existence_pokemon_sql = '''
     SELECT name
@@ -71,7 +82,64 @@ def pokemon_table_schema_is_valid():
     if expected != result:
         return False
 
-    sqlite3_cursor.close()
-    sqlite3_conn.close()
+    return True
+
+
+def moves_table_schema_is_valid(
+        sqlite3_conn,
+        sqlite3_cursor
+):
+    """Validates SQLite table 'moves' and panics if result table schema
+    does not match the expected table schema.
+
+    Since method call is highly dependent on what the SQLite table schema
+    is, ensure alignment between stateful db table generation, and stateful
+    ingestion from third-party API, and checkpoint the data pipeline to
+    diagnose any failures.
+
+    Args:
+        sqlite3_conn (sqlite3.Connection)
+        sqlite3_cursor (sqlite3.Cursor)
+
+    Returns:
+        bool: SQLite table 'moves' exists and has expected schema.
+    """
+    # Check that the table 'moves' exists. #
+    existence_moves_sql = '''
+    SELECT name
+    FROM sqlite_master
+    WHERE type='table'
+    AND name='moves';
+    '''
+    sqlite3_cursor.execute(existence_moves_sql)
+    result = sqlite3_cursor.fetchone()[0]
+
+    if result != 'moves':
+        return False
+
+    # Check that the table 'moves' has the expected schema. #
+    describe_moves_sql = '''
+    SELECT sql
+    FROM sqlite_master
+    WHERE name = 'moves';
+    '''
+    sqlite3_cursor.execute(describe_moves_sql)
+    result = sqlite3_cursor.fetchone()[0]
+    result = re.sub('[^0-9a-zA-Z\(\)]', ' ', result)
+    result = ' '.join(result.split())
+
+    expected = '''
+    CREATE TABLE moves
+    (
+        id INTEGER PRIMARY KEY NOT NULL,
+        type NVARCHAR(256) NOT NULL,
+        accuracy INTEGER NOT NULL
+    );
+    '''
+    expected = re.sub('[^0-9a-zA-Z\(\)]', ' ', expected)
+    expected = ' '.join(expected.split())
+
+    if expected != result:
+        return False
 
     return True
